c26e119925492ed0a2c3ff75dcd3832f95162dfb
Neha Sontakke <nsontakke004@gmail.com>
2024-08-17 10:05:17 -0700

    Update multibin_prokka_tantan_bathsearch.sh

--- BATHsearch_Multiple_Bins/multibin_prokka_tantan_bathsearch.sh
+++ BATHsearch_Multiple_Bins/multibin_prokka_tantan_bathsearch.sh
@@ -8,67 +8,71 @@
 #SBATCH --account=twheeler
 #SBATCH --partition=standard
 
-# Specify the bin file
-bin_files=("bin.82""bin.16""bin.329""bin.121""bin.40""bin.104")
+# Specify the bin files
+bin_files=("bin.82" "bin.16" "bin.329" "bin.121" "bin.40" "bin.104")
 
 for bin_file in "${bin_files[@]}"; 
 do
 (
+    echo "Process started at $(date) on ${bin_file}"
+    base_dir="/xdisk/twheeler/nsontakke/BATHSEARCH_PARALLEL/"
+    # Path to the DNA input file
+    input_file="${base_dir}/MAG_Data/${bin_file}.fa"
+    #Add the prokka running instructions
+    singularity exec /home/u13/nsontakke/prokka.sif prokka --outdir Prokka_output_${bin_file} --prefix ${bin_file} ${input_file}
+    echo "Prokka on ${bin_file} ends at $(date)"
+    #Add the prokka running instructions
 
-#Add the prokka running instructions
-singularity exec /home/u13/nsontakke/prokka.sif prokka --outdir Prokka_output --prefix ${bin_file} /xdisk/twheeler/nsontakke/Prokka_BATH_Comparison_2/MAG_Data/${bin_file}
+    # TANTAN
+    tantan_exec="/xdisk/twheeler/nsontakke/Software/tantan-49/bin/tantan"
+    masked_dna_file="${base_dir}/MAG_Data/${bin_file}.masked.fa"
+    # Run Tantan for masking DNA
+    ${tantan_exec} -x N ${input_file} > ${masked_dna_file}
 
-echo "Process started at $(date)"
+    #THIS ASSUMES OTHER TANTAN COMMANDS FROM tantan_bathsearch.sh HAVE BEEN EXECUTED BEFORE
+    masked_protein_dir="/xdisk/twheeler/nsontakke/Prokka_BATH_Comparison_2/Input_Bathsearch/masked_proteins"
 
-# Path to the DNA input file
-input_file="/xdisk/twheeler/nsontakke/Prokka_BATH_Comparison_2/MAG_Data/${bin_file}.fa"
+    # BATH SEARCH
+    bathsearch_exec="/home/u13/nsontakke/BATH/src/bathsearch"
+    output_main_dir="${base_dir}/Output_Bathsearch_${bin_file}"
+    mkdir -p ${output_main_dir}
 
-# TANTAN
-tantan_exec="/xdisk/twheeler/nsontakke/Software/tantan-49/bin/tantan"
-masked_dna_file="/xdisk/twheeler/nsontakke/Prokka_BATH_Comparison_2/MAG_Data/${bin_file}.masked.fa.fna"
+    # Run Bacteria
+    ${bathsearch_exec} --ct 11 --hmmout ${output_main_dir}/${bin_file}_DNA_Bacteria_kingdom_sprot.fhmm --tblout ${output_main_dir}/${bin_file}_DNA_Bacteria_kingdom_sprot.tbl -o ${output_main_dir}/${bin_file}_DNA_Bacteria_kingdom_sprot.out ${masked_protein_dir}/Bacteria/sprot.masked ${masked_dna_file} &
 
-# Run Tantan for masking DNA
-${tantan_exec} -x N ${input_file} > ${masked_dna_file}
+    # Run Archaea
+    ${bathsearch_exec} --ct 11 --hmmout ${output_main_dir}/${bin_file}_DNA_Archaea_kingdom_sprot.fhmm --tblout ${output_main_dir}/${bin_file}_DNA_Archaea_kingdom_sprot.tbl -o ${output_main_dir}/${bin_file}_DNA_Archaea_kingdom_sprot.out ${masked_protein_dir}/Archaea/sprot.masked ${masked_dna_file} &
 
-# BATH SEARCH
-bathsearch_exec="/home/u13/nsontakke/BATH/src/bathsearch"
-output_main_dir="/xdisk/twheeler/nsontakke/Prokka_BATH_Comparison_2/Output_Bathsearch_${bin_file}"
-mkdir -p ${output_main_dir}
+    # Run HAMAP
+    ${bathsearch_exec} -o ${output_main_dir}/${bin_file}_HAMAP_bath.out --tblout ${output_main_dir}/${bin_file}_HAMAP_bath.tbl /xdisk/twheeler/nsontakke/Prokka_BATH_Comparison_2/Input_Bathsearch/HAMAP_ALL.bhmm ${masked_dna_file} &
 
-# Run Bacteria
-${bathsearch_exec} --ct 11 --hmmout ${output_main_dir}/${bin_file}_DNA_Bacteria_kingdom_sprot.fhmm --tblout ${output_main_dir}/${bin_file}_DNA_Bacteria_kingdom_sprot.tbl -o ${output_main_dir}/${bin_file}_DNA_Bacteria_kingdom_sprot.out ${masked_protein_dir}/Bacteria/sprot.masked ${masked_dna_file} &
+    # Run on Bacteria/IS
+    ${bathsearch_exec} --ct 11 --hmmout ${output_main_dir}/${bin_file}_DNA_Bacteria_IS_sprot.fhmm --tblout ${output_main_dir}/${bin_file}_DNA_Bacteria_IS_sprot.tbl -o ${output_main_dir}/${bin_file}_DNA_Bacteria_IS_sprot.out ${masked_protein_dir}/Bacteria/IS.masked ${masked_dna_file} &
 
-# Run Archaea
-${bathsearch_exec} --ct 11 --hmmout ${output_main_dir}/${bin_file}_DNA_Archaea_kingdom_sprot.fhmm --tblout ${output_main_dir}/${bin_file}_DNA_Archaea_kingdom_sprot.tbl -o ${output_main_dir}/${bin_file}_DNA_Archaea_kingdom_sprot.out ${masked_protein_dir}/Archaea/sprot.masked ${masked_dna_file} &
+    # Run on Bacteria/AMR
+    ${bathsearch_exec} --ct 11 --hmmout ${output_main_dir}/${bin_file}_DNA_Bacteria_AMR_sprot.fhmm --tblout ${output_main_dir}/${bin_file}_DNA_Bacteria_AMR_sprot.tbl -o ${output_main_dir}/${bin_file}_DNA_Bacteria_AMR_sprot.out ${masked_protein_dir}/Bacteria/AMR.masked ${masked_dna_file} &
 
-# Run HAMAP
-${bathsearch_exec} -o ${output_main_dir}/${bin_file}_HAMAP_bath.out --tblout ${output_main_dir}/${bin_file}_HAMAP_bath.tbl /xdisk/twheeler/nsontakke/Prokka_BATH_Comparison_2/Input_Bathsearch/HAMAP_ALL.bhmm ${masked_dna_file} &
+    # Run viral commands with codon tables 1 and 11
+    for ct in 1 11; do
+        hmmout="${output_main_dir}/${bin_file}_DNA_Viruses_kingdom_sprot_ct${ct}.fhmm"
+        tblout="${output_main_dir}/${bin_file}_DNA_Viruses_kingdom_sprot_ct${ct}.tbl"
+        output="${output_main_dir}/${bin_file}_DNA_Viruses_kingdom_sprot_ct${ct}.out"
+        ${bathsearch_exec} --ct ${ct} --hmmout ${hmmout} --tblout ${tblout} -o ${output} ${masked_protein_dir}/Viruses/sprot.masked ${masked_dna_file} &
+    done
 
-# Run on Bacteria/IS
-${bathsearch_exec} --ct 11 --hmmout ${output_main_dir}/${bin_file}_DNA_Bacteria_IS_sprot.fhmm --tblout ${output_main_dir}/${bin_file}_DNA_Bacteria_IS_sprot.tbl -o ${output_main_dir}/${bin_file}_DNA_Bacteria_IS_sprot.out ${masked_protein_dir}/Bacteria/IS.masked ${masked_dna_file} &
+    # Run bathsearch for each mitochondrial codon table
+    declare -a codon_tables=(2 3 4 5 9 13 14 16 21 22 23 24 25)
+    for ct in "${codon_tables[@]}"; do
+        hmmout="${output_main_dir}/${bin_file}_DNA_Mitochondria_kingdom_sprot_ct${ct}.fhmm"
+        tblout="${output_main_dir}/${bin_file}_DNA_Mitochondria_kingdom_sprot_ct${ct}.tbl"
+        output="${output_main_dir}/${bin_file}_DNA_Mitochondria_kingdom_sprot_ct${ct}.out"
+        ${bathsearch_exec} --ct ${ct} --hmmout ${hmmout} --tblout ${tblout} -o ${output} ${masked_protein_dir}/Mitochondria/sprot.masked ${masked_dna_file} &
+    done
+    echo "Bathsearch completed at $(date)" &
+    wait
 
-# Run on Bacteria/AMR
-${bathsearch_exec} --ct 11 --hmmout ${output_main_dir}/${bin_file}_DNA_Bacteria_AMR_sprot.fhmm --tblout ${output_main_dir}/${bin_file}_DNA_Bacteria_AMR_sprot.tbl -o ${output_main_dir}/${bin_file}_DNA_Bacteria_AMR_sprot.out ${masked_protein_dir}/Bacteria/AMR.masked ${masked_dna_file} &
 
-# Run viral commands with codon tables 1 and 11
-for ct in 1 11; do
-    hmmout="${output_main_dir}/${bin_file}_DNA_Viruses_kingdom_sprot_ct${ct}.fhmm"
-    tblout="${output_main_dir}/${bin_file}_DNA_Viruses_kingdom_sprot_ct${ct}.tbl"
-    output="${output_main_dir}/${bin_file}_DNA_Viruses_kingdom_sprot_ct${ct}.out"
-    ${bathsearch_exec} --ct ${ct} --hmmout ${hmmout} --tblout ${tblout} -o ${output} ${masked_protein_dir}/Viruses/sprot.masked ${masked_dna_file} &
-done
-
-# Run bathsearch for each mitochondrial codon table
-declare -a codon_tables=(2 3 4 5 9 13 14 16 21 22 23 24 25)
-for ct in "${codon_tables[@]}"; do
-    hmmout="${output_main_dir}/${bin_file}_DNA_Mitochondria_kingdom_sprot_ct${ct}.fhmm"
-    tblout="${output_main_dir}/${bin_file}_DNA_Mitochondria_kingdom_sprot_ct${ct}.tbl"
-    output="${output_main_dir}/${bin_file}_DNA_Mitochondria_kingdom_sprot_ct${ct}.out"
-    ${bathsearch_exec} --ct ${ct} --hmmout ${hmmout} --tblout ${tblout} -o ${output} ${masked_protein_dir}/Mitochondria/sprot.masked ${masked_dna_file} &
-done
-echo "Bathsearch completed at $(date)" &
-wait
 ) &
 done
 wait
-echo"All bin files processed."
+echo "All bin files processed." 
